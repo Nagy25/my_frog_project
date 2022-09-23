28598836208df0ec9b23223ce971384f3b24b72e
ahmed nagy <ahmednagy@kortobaa.com>
2022-09-23 21:21:37 +0200

    Initial commit

---
+++ routes/hello.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:dart_frog/dart_frog.dart';
+
+Future<Response> onRequest(RequestContext context) async {
+  // Access the incoming request.
+  final request = context.request;
+  // Access the HTTP method.
+  final method = request.method.value;
+
+  final headers = request.headers;
+
+  final params = request.uri.queryParameters;
+  final name = params['name'] ?? 'no name ';
+
+  // Access the request body as a `String`.
+  final body = await request.body();
+  final data = json.decode(body) as Map<String, dynamic>;
+  final theName = data['name'];
+
+  return Response(
+    body: 'Welcome to hello2  page! method is $method \n and the headers is '
+        '$headers \n \n and queryParameters name  is $name \n \n '
+        'and body is $theName',
+  );
+}
