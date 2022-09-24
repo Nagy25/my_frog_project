f2317f0706eb93b3d72017b4464f1e5226aa2dbc
ahmed nagy <ahmednagy@kortobaa.com>
2022-09-24 08:58:52 +0200

    response and dynamic middleware

---
+++ routes/_middleware.dart
@@ -0,0 +1,21 @@
+import 'package:dart_frog/dart_frog.dart';
+
+Handler middleware(Handler handler) {
+  // logger for all routs
+  //return handler.use(requestLogger());
+  return (context) async {
+    // Execute code before request is handled.
+
+    final request = context.request;
+    final params = request.uri.queryParameters;
+    final name = params['name'] ?? '';
+
+    // Forward the request to the respective handler.
+    final response = await handler(context);
+
+    // Execute code after request is handled.
+
+    // Return a response.
+    return name.isNotEmpty ? response : Response(statusCode: 401);
+  };
+}
